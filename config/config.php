62136f4440af76b033f2d74fa5e86646cec1fe6a
Roman <rromanishin@ucell.com.ua>
2018-09-06 18:40:14 +0300

    liq pay fix

--- config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return array(
-    'mail_user'=> 'romanrimskiy@gmail.com',
-    'mail_password'=> 'qaz321wsx',
-    'LIQPAY_PRIVATE_KEY'=>'yHvaJcudRn07m0Ef7NVrYZwlhdYIl5sjvFVF77Ag',
-    'LIQPAY_PUBLIC_KEY'=>'i72350480191'
-);
