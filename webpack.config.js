430e53bcafc002a2d5ae36cb375d78811becddcf
roman <romanrimskiy@gmail.com>
2019-07-03 11:39:44 +0300

    add server

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,9 @@ module.exports = (env) => {
 
     return     {
         entry: './src/index.js',
+        performance: {
+            hints: false
+          },
         output: {
             path: path.join(__dirname, 'public', 'dist'),
             filename: 'bundle.js',
@@ -40,7 +43,7 @@ module.exports = (env) => {
                                 sourceMap: true,
                             }
                         },
-                        'resolve-url-loader',
+                        // 'resolve-url-loader',
                         {
                             loader: 'sass-loader',
                             options:{
@@ -86,7 +89,7 @@ module.exports = (env) => {
             })
         ],
         devServer: {
-            contentBase:  path.join(__dirname, 'public'),
+            contentBase:  path.join(__dirname, './public'),
             historyApiFallback: true,
             publicPath: '/dist/'
         }
